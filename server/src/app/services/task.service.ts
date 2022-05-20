1e6cc30b82c6fb9c805bffd9edfc3d44e0f831f4
Breno Lopes <brenokos@gmail.com>
2022-05-20 02:31:11 -0300

    feat: add TaskService class

---
+++ server/src/app/services/task.service.ts
@@ -0,0 +1,54 @@
+import Service from './service.abstract';
+import { 
+  taskSchema,
+  idSchema,
+  TasksTypes,
+  DomainError,
+  StatusCode,
+  MessageErrors } from '../helpers';
+import { ServiceError } from '../interfaces';
+import { TaskModel } from '../models';
+
+// T = Task
+// só subscreva métodos que precisam de um tipo de validação, como create ou update/delete 
+
+export default class TaskService extends Service<TasksTypes> {
+  constructor(model = new TaskModel()) {
+    super(model);
+  }
+
+  create = async (
+    obj: TasksTypes,
+  ): Promise<TasksTypes | ServiceError | null> => {
+    taskSchema.parse(obj);
+    
+    return this.model.create(obj);
+  };
+
+  update = async (
+    id: string,
+    obj: TasksTypes,
+  ): Promise<TasksTypes | ServiceError | null> => {
+    idSchema.parse(id);
+    taskSchema.parse(obj);
+
+    const task = await this.model.readOne(id);
+
+    if (!task) {
+      throw new DomainError(StatusCode.NOT_FOUND, MessageErrors.TASK_NOT_FOUND);
+    }
+
+    return this.model.update(id, obj);
+  };
+
+  delete = async (id: string): Promise<TasksTypes | ServiceError | null> => {
+    idSchema.parse(id);
+
+    const task = await this.model.readOne(id);
+    
+    if (!task) {
+      throw new DomainError(StatusCode.NOT_FOUND, MessageErrors.TASK_NOT_FOUND);
+    }
+    return this.model.delete(id);
+  };
+}
