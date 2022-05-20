47b4d4c0670c081ed513d41976a92d7a0186da6c
Breno Lopes <brenokos@gmail.com>
2022-05-20 02:36:14 -0300

    feat: add middleware barrel (index.ts)

---
+++ server/src/api/middlewares/index.ts
@@ -0,0 +1,5 @@
+import zodMiddleware from './zod.error';
+import domainMiddleware from './domain.error';
+import internalMiddleware from './internal.error';
+
+export { zodMiddleware, domainMiddleware, internalMiddleware };
