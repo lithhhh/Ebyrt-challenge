605487f960e79637b9721615bfcef17d1a62ba41
Breno Lopes <brenokos@gmail.com>
2022-05-20 02:24:30 -0300

    feat: add barrel (index.ts)

---
+++ server/src/app/helpers/index.ts
@@ -0,0 +1,10 @@
+import { taskSchema, TasksTypes, idSchema } from './zod.validation';
+import StatusCode from './status.enum';
+import MessageErrors from './messages.enum';
+import { ResponseError, TypeDomainError } from './error.type';
+import { DomainError, isDomainError } from './domain.error';
+
+export {
+  taskSchema, TasksTypes, StatusCode, MessageErrors, ResponseError, DomainError,
+  TypeDomainError, isDomainError, idSchema,
+};
