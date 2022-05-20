9134f510c9c4294a2fa772f9873e3587f048e4b4
Breno Lopes <brenokos@gmail.com>
2022-05-20 02:29:24 -0300

    feat: add abstract service

---
+++ server/src/app/services/service.abstract.ts
@@ -0,0 +1,26 @@
+import { Model } from '../../database/interfaces';
+import { ServiceError } from '../interfaces';
+
+export default abstract class Service<T> {
+  constructor(protected model: Model<T>) {}
+
+  public async create(obj: T): Promise<T | null | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  public async read(): Promise<T[] | null | ServiceError> {
+    return this.model.read();
+  }
+
+  public async update(id: string, obj: T): Promise<T | null | ServiceError> {
+    return this.model.update(id, obj);
+  }
+
+  public readOne(id: string): Promise<T | ServiceError | null> {
+    return this.model.readOne(id);
+  }
+
+  public async delete(id: string): Promise<T | null | ServiceError> {
+    return this.model.delete(id);
+  }
+}
