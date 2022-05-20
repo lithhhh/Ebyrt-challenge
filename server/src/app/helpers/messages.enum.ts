98c543a6122cff14db0937423736567d71e6d045
Breno Lopes <brenokos@gmail.com>
2022-05-20 02:21:34 -0300

    feat: add response messages enum

---
+++ server/src/app/helpers/messages.enum.ts
@@ -0,0 +1,8 @@
+enum MessageErrors {
+  INTERNAL_SERVER_ERROR = 'Internal Server Error',
+  NOT_FOUND = 'Object not found',
+  TASK_NOT_FOUND = 'Task not found',
+  ID_REQUIRED = 'Id is required',
+  BAD_REQUEST = 'Bad request',
+}
+export default MessageErrors;
