900944d8447afcb1d904b3f49f6ca6118a302340
Breno Lopes <brenokos@gmail.com>
2022-05-20 02:09:40 -0300

    feat: mongoose connection setup

---
+++ server/src/database/connection.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const MONGO_DB_URL_LOCALHOST = 'mongodb://localhost:27017/Ebyrt-to-do';
+
+const connectToDatabase = (
+  mongoDatabaseURI = process.env.MONGO_URI
+    || MONGO_DB_URL_LOCALHOST,
+) => {
+  console.log('conecta?');
+  
+  return mongoose.connect(mongoDatabaseURI);
+};
+
+export default connectToDatabase;
