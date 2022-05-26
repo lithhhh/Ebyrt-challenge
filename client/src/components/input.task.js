b219d79a96135242a731510759b52a2fbbced055
Breno Lopes <brenokos@gmail.com>
2022-05-26 13:48:13 -0300

    feat: add route main (and setup main page)

---
+++ client/src/components/input.task.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Input() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="digite aqui sua task" />
+        <input type="submit" value="adicionar" />
+      </form>
+    </div>
+  );
+}
+
+export default Input;
