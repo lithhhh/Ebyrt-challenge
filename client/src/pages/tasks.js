f99aa10871fba38ea6a5de46e6094c5a008e6458
Breno Lopes <brenokos@gmail.com>
2022-05-26 13:48:13 -0300

    feat: add route main (and setup main page)

---
+++ client/src/pages/tasks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Input from '../components/input.task';
+
+function Tasks() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
+
+export default Tasks;
