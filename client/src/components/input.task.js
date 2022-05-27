03487e685c7347d969bc27ea8f91289a5c184ec2
Breno Lopes <brenokos@gmail.com>
2022-05-27 17:07:32 -0300

    feat: add input.task component

--- client/src/components/input.task.js
+++ client/src/components/input.task.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import api from '../utils/api';
+import TaskContext from '../context/myContext';
+
+const taskSchema = {
+  title: '',
+};
 
 function Input() {
+  const [task, setTask] = useState(taskSchema);
+  const { setTasks, tasks } = useContext(TaskContext);
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const req = await api.post('/task', {
+        ...task,
+      });
+      setTasks((cur) => [...cur, task]);
+      setTask(taskSchema);
+      console.log(req);
+      console.log(tasks);
+    } catch (err) {
+      console.log(err);
+      alert('houve um erro ao adicionar a task.');
+    }
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    setTask({
+      ...task,
+      [e.target.name]: value,
+    });
+  };
+
+  useEffect(() => console.log(tasks), [tasks]);
   return (
     <div>
-      <form>
-        <input type="text" placeholder="digite aqui sua task" />
+      <form onSubmit={ handlerSubmit }>
+        <input
+          type="text"
+          placeholder="digite aqui sua task"
+          name='title'
+          onChange={ handleChange }
+          value={ task.title }
+          />
         <input type="submit" value="adicionar" />
       </form>
     </div>
