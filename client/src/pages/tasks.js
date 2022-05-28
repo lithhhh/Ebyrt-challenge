f17cf86c5c5eb0f148f7532924335c811bd0b470
Breno Lopes <brenokos@gmail.com>
2022-05-28 17:14:09 -0300

    feat: assembling cards on the task page

--- client/src/pages/tasks.js
+++ client/src/pages/tasks.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Row } from 'react-bootstrap';
 
 import Input from '../components/input.task';
+import CardTask from '../components/card.task';
+import myContext from '../context/myContext';
 
 function Tasks() {
+  const { tasks } = useContext(myContext);
+
   return (
     <div>
       <Input />
+      <Row xs={1} md={3} className="g-4">
+        {Array.from(tasks).map(({
+          title, color, details, status,
+        }, idx) => (
+          <CardTask
+            key={ idx }
+            title={ title }
+            color={ color }
+            details={ details }
+            status={ status }
+          />
+        ))}
+      </Row>
     </div>
   );
 }
