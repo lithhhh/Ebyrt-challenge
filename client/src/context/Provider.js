b1e8f9d3ba59ce85345dffa4c3455bdfe071efe5
Breno Lopes <brenokos@gmail.com>
2022-05-28 17:11:11 -0300

    feat: GET /task request set to provider

--- client/src/context/Provider.js
+++ client/src/context/Provider.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 
+import api from '../utils/api';
 import AppContext from './myContext';
 
 function Provider({ children }) {
   const [tasks, setTasks] = useState([]);
+
+  const getTasks = async () => {
+    try {
+      const { data } = await api.get('/task');
+      setTasks(data);
+    } catch (err) {
+      console.log(err);
+      alert('houve um erro ao adicionar ao buscar suas tarefas.');
+    }
+  };
+
+  useLayoutEffect(() => {
+    getTasks();
+  }, []);
 
   const contextValues = {
     setTasks,
