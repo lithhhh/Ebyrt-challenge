2a660ee53a0da873366ca4338c150a00a39d502b
Breno Lopes <brenokos@gmail.com>
2022-05-28 17:13:26 -0300

    feat: set task card component

---
+++ client/src/components/card.task.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button, ButtonGroup, Card, Col, Dropdown, DropdownButton,
+} from 'react-bootstrap';
+
+function CardTask({
+  title, details, status, color,
+}) {
+  // const [taskId] = useState(id);
+  return (
+    <Col>
+      <Card style={{ backgroundColor: color }}>
+        <Card.Header>{ title }</Card.Header>
+        <Card.Body>
+          {!details && <Card.Subtitle className="mb-2 text-muted">Sem detalhes</Card.Subtitle>}
+          <Card.Text>
+            { details }
+          </Card.Text>
+          <ButtonGroup className="d-flex">
+            <Button>Editar</Button>
+            <Button>Deletar</Button>
+              <DropdownButton as={ButtonGroup} title={ status } id="bg-nested-dropdown">
+              <Dropdown.Item eventKey="1">Pendente</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Em andamento</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Finalizado</Dropdown.Item>
+            </DropdownButton>
+          </ButtonGroup>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+CardTask.propTypes = {
+  title: PropTypes.string.isRequired,
+  details: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+export default CardTask;
