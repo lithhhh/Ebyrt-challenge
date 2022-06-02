15c41dc6b957238c92c5a3f0ec6379fb3d9071f7
Breno Lopes <brenokos@gmail.com>
2022-06-02 20:59:28 -0300

    feat: add updateStatus (controller)

--- server/src/app/controller/task.controller.ts
+++ server/src/app/controller/task.controller.ts
@@ -1,9 +1,12 @@
 import Controller from './controller.abstract';
 import { TaskService } from '../services';
 import { TasksTypes } from '../helpers';
+import { ServiceError } from '../interfaces';
 
 export default class TaskController extends Controller<TasksTypes> {
   route: string;
+
+  private taskService: TaskService;
 
   constructor(
     service = new TaskService(),
@@ -11,5 +14,12 @@ export default class TaskController extends Controller<TasksTypes> {
   ) {
     super(service);
     this.route = route;
+    this.taskService = service;
   }
+
+  updateStatus = async (
+    id: string,
+    obj: TasksTypes,
+  ): Promise<TasksTypes | ServiceError | null> => this
+    .taskService.updateStatus(id, obj);
 }
