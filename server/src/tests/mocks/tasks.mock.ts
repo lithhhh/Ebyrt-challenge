044842e601aa26b18ca4f934ca6598dd67bb852c
Breno Lopes <brenokos@gmail.com>
2022-06-05 17:36:49 -0300

    chore: add task mocks

---
+++ server/src/tests/mocks/tasks.mock.ts
@@ -0,0 +1,32 @@
+export default {
+  tasks: [
+    {
+      id: '1',
+      title: 'task do breno',
+      color: '#ffffff',
+      status: 'Pendente',
+      details: ''
+    },
+    {
+      id: '2',
+      title: 'task da bruna',
+      color: '#ffffff',
+      status: 'Em andamento',
+      details: ''
+    },
+    {
+      id: '3',
+      title: 'task da bya',
+      color: '#ffffff',
+      status: 'Finalizado',
+      details: ''
+    }
+  ],
+
+  creatingATask: {
+    title: 'task do breno',
+    color: '#ffffff',
+    status: 'Pendente',
+    details: ''
+  }
+}
