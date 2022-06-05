e659348dcd3a8ea53bf0ef5323fec09405b58ae7
Breno Lopes <brenokos@gmail.com>
2022-06-05 14:56:52 -0300

    fix: fixes status validation values in zod.validation (app layer)

--- server/src/app/helpers/zod.validation.ts
+++ server/src/app/helpers/zod.validation.ts
@@ -4,13 +4,13 @@ const taskSchema = z.object({
   title: z.string().min(5),
   color: z.string()
     .regex(/^#(?:(?:[\da-f]{3}){1,2}|(?:[\da-f]{4}){1,2})$/i).optional(),
-  status: z.enum(['pendente', 'Em andamento', 'pronto']).optional(),
+  status: z.enum(['Pendente', 'Em andamento', 'Finalizado']).optional(),
   details: z.string().optional(),
 });
 
 const idSchema = z.string().min(1);
 
-const statusSchema = z.enum(['pendente', 'Em andamento', 'pronto']);
+const statusSchema = z.enum(['Pendente', 'Em andamento', 'Finalizado']);
 
 type TasksTypes = z.infer<typeof taskSchema>;
 
