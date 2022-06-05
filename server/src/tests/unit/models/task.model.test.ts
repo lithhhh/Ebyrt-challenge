2bf98a8499616451b9f245b7a3dc8aff269b5287
Breno Lopes <brenokos@gmail.com>
2022-06-05 17:36:08 -0300

    test: add task.model test skeleton

---
+++ server/src/tests/unit/models/task.model.test.ts
@@ -0,0 +1,29 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { test } from 'mocha';
+import { Model } from 'mongoose';
+
+import { TaskModel } from '../../../app/models';
+import { TasksTypes } from '../../../app/helpers';
+import { tasksMock } from '../../mocks';
+
+describe('task.model', () => {
+  const tasks = new TaskModel();
+
+  describe('create', () => {
+
+  });
+
+  describe('delete', () => {
+
+  });
+
+  describe('update', () => {
+
+  });
+  describe('read', () => {
+    test('returns an array of objects containing the required keys', async () => {
+
+    });
+  });
+});
