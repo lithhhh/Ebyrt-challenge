aff26ec1f7449fabfeae407b2c7ba4fc71b5bd4d
Breno Lopes <brenokos@gmail.com>
2022-06-10 02:56:29 -0300

    refactor: mongo.model.interface adapted

--- server/src/database/interfaces/mongo.model.interface.ts
+++ server/src/database/interfaces/mongo.model.interface.ts
@@ -1,6 +1,6 @@
 interface Model<T> {
   create(obj: T): Promise<T>,
-  read(): Promise<T[]>,
+  read(query?: string | null): Promise<T[]>,
   update(id: string, obj: T): Promise<T | null>
   readOne(str: string): Promise<T | null>
   delete(id: string): Promise<T | null>
