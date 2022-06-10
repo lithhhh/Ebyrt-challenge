201d9c883916e72e76610cd23910362d0c39ea1f
Breno Lopes <brenokos@gmail.com>
2022-06-10 01:26:59 -0300

    chore: merge

--- server/src/tests/mocks/tasks.mock.ts
+++ server/src/tests/mocks/tasks.mock.ts
@@ -28,28 +28,5 @@ export default {
     color: '#ffffff',
     status: 'Pendente',
     details: ''
-  },
-
-  wrongTask: {
-    title: 'abc',
-    color: '#ffffff',
-    status: 'Pendente',
-    details: ''
-  },
-
-  statusUpdatedOne: {
-    id: '1',
-    title: 'task do breno',
-    color: '#ffffff',
-    status: 'Em andamento',
-    details: ''
-  },
-
-  statusUpdatedTwo: {
-    id: '2',
-    title: 'task da bruna',
-    color: '#ffffff',
-    status: 'Finalizado',
-    details: ''
-  },
+  }
 }
